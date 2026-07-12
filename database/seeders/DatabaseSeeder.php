65f178d22b20135fcbe95bb1d62958643c9a647a
Yael2924 <saulyaeld@gmail.com>
2026-07-12 01:43:14 -0600

    Autenticacion
    Implementar autenticacion y usuario inicial

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategoriaSeeder::class,
             LibroSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,44 @@ use App\Http\Controllers\LibroController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\PrestamoController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return redirect('/dashboard');
 });
 
-Route::get('/dashboard', [DashboardController::class,'index'])
+Route::middleware('auth')->group(function(){
+
+    Route::get('/dashboard', [DashboardController::class,'index'])
     ->name('dashboard');
 
-Route::get('/libros', [LibroController::class, 'index'])
-    ->name('libros.index');
+    Route::get('/libros', [LibroController::class, 'index'])
+        ->name('libros.index');
 
-Route::get('/categorias', [CategoriaController::class, 'index'])
-    ->name('categorias.index');
+    Route::get('/categorias', [CategoriaController::class, 'index'])
+        ->name('categorias.index');
 
- Route::get('/prestamos', [PrestamoController::class, 'index'])
-    ->name('prestamos.index');
+    Route::get('/prestamos', [PrestamoController::class, 'index'])
+        ->name('prestamos.index');
 
-Route::get('/usuarios', [UsuarioController::class, 'index'])
-    ->name('usuarios.index');
+    Route::get('/usuarios', [UsuarioController::class, 'index'])
+        ->name('usuarios.index');
+
+});
+
+Route::get('/login', 
+    [AuthController::class,'login']
+)
+->name('login');
+
+
+Route::post('/login', 
+    [AuthController::class,'authenticate']
+)
+->name('login.authenticate');
+
+
+Route::post('/logout',
+    [AuthController::class,'logout']
+)
+->name('logout');
